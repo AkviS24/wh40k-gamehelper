1892b1d5741fd392c886c70a460888045ff604f3
Akvis24 <akvis2022@gmail.com>
2026-05-17 08:16:50 +0200

    created Impressum and Datasecure Policy and created the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainPage} from './shared/pages/main-page/main-page';
+import { Impressum } from './shared/pages/impressum/impressum';
+import { Datenschutz } from './shared/pages/datenschutz/datenschutz';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:MainPage},
+    {path: '**', redirectTo: ''},
+    {path: 'impressum', component:Impressum},
+    {path: 'datenschutz', component:Datenschutz},
+];
