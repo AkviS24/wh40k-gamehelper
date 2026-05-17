d296bb6df4194ea23c922fd69dbcc806b8e2f789
Akvis24 <akvis2022@gmail.com>
2026-05-17 08:09:28 +0200

    global styles added and first structure and components build

---
+++ src/app/shared/components/navigation-bar/navigation-bar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-bar',
+  imports: [],
+  templateUrl: './navigation-bar.html',
+  styleUrl: './navigation-bar.scss',
+})
+export class NavigationBar {}
